7029840e9af8dc988ded8f9777ca9b3e15dba764
danial137 <danial79fakhrabadi@gmail.com>
2025-12-15 17:05:41 +0330

    refactor: improve HomeCard component structure and styling

--- components/quiz/HomeCard.tsx
+++ components/quiz/HomeCard.tsx
@@ -1,25 +1,36 @@
-import { ICategory } from '@/types/types'
-import Image from 'next/image'
-import React from 'react'
+import { ICategory } from "@/types/types";
+import Image from "next/image";
+import React from "react";
 
 interface Props {
-    category: ICategory
+    category: ICategory;
 }
 
+const HomeCard = ({ category }: Props) => {
+    const imageSrc = category.image
+        ? category.image
+        : `/categories/image--${category.name
+            .toLowerCase()
+            .replace(/\s+/g, "-")}.svg`;
 
-const HomeCard = ({ category }:Props) => {
     return (
-        <div className='border-2 rounded-xl p-1 cursor-pointer shadow-[0_.3rem_0_0_rgba(0,0,0,0.1) hover:-translate-y-1 transition-transform duration-300 ease-in-out]'>
-            <div className='rounded-xl h-[9rem] py-1'>
-                <Image src={category.image ? category.image : `/categories/image--${category.name.toLowerCase().split("").join("-")}.svg`}
+        <div className="border-2 rounded-xl p-1 cursor-pointer shadow-[0_.3rem_0_0_rgba(0,0,0,0.1)] hover:-translate-y-1 transition-transform duration-300 ease-in-out">
+            <div className="rounded-xl h-[9rem] py-1 flex items-center justify-center">
+                <Image
+                    src={imageSrc}
                     alt={category.name}
-                    layout='fill'
-                    objectFit='cover'
-                    className='rounded-xl'
+                    width={300}
+                    height={200}
+                    className="h-full  rounded-xl"
+                    priority
                 />
             </div>
-        </div>
-    )
-}
 
-export default HomeCard
+            <div>
+                <h2>{category.name}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default HomeCard;
